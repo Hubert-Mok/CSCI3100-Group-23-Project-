69ab455e8a41209d5515355b3bba6160200c876c
jaccokwok <chihimkwok426@gmail.com>
2026-04-12 05:52:12 +0800

    Added Rspec test for product creation

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'bundler/setup'
+require 'rspec'
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
