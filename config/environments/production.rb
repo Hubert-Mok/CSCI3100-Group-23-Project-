f5af3e4becd1cab0e2207bc780cc9aa189c8b90e
Hubert Mok <hubertmok@HubertdeMacBook-Pro.local>
2026-04-14 01:03:21 +0800

    Remove separate queue database connection for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  #config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Raise delivery errors in production so failures surface in logs/error tracking.
   config.action_mailer.raise_delivery_errors = true
