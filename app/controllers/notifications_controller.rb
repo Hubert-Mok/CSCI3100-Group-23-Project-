1524ea897911ea4fe22f90f8b89251666e2fe9de
jaccokwok <chihimkwok426@gmail.com>
2026-04-13 23:45:05 +0800

    Refactored notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   before_action :require_login
   before_action :set_notification, only: %i[update destroy]
+  after_action :broadcast_notification_badge, only: %i[destroy clear_all]
 
   def index
     @notifications = current_user.notifications.recent
@@ -8,36 +9,33 @@ class NotificationsController < ApplicationController
 
   def update
     @notification.update(read: true)
-
-    respond_to do |format|
-      format.html { redirect_back fallback_location: notifications_path }
-      format.turbo_stream
-    end
+    respond_with_notifications
   end
 
   def destroy
     @notification.destroy
-    broadcast_notification_badge_to(current_user)
-
-    respond_to do |format|
-      format.html { redirect_back fallback_location: notifications_path }
-      format.turbo_stream
-    end
+    respond_with_notifications
   end
 
   def clear_all
     current_user.notifications.delete_all
-    broadcast_notification_badge_to(current_user)
-
-    respond_to do |format|
-      format.html { redirect_back fallback_location: notifications_path }
-      format.turbo_stream
-    end
+    respond_with_notifications
   end
 
   private
 
   def set_notification
     @notification = current_user.notifications.find(params[:id])
+  end
+
+  def respond_with_notifications
+    respond_to do |format|
+      format.html { redirect_back fallback_location: notifications_path }
+      format.turbo_stream
+    end
+  end
+
+  def broadcast_notification_badge
+    broadcast_notification_badge_to(current_user)
   end
 end
